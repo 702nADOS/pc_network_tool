ac650b41cacd4f803bbee2e7b5ea6c025751948d
Avinash <avinash@avinash.com.np>
2015-08-05 18:46:01 +0200

    add eventEmiter model and helper functions to dom0

--- dom0_client.js
+++ dom0_client.js
@@ -1,17 +1,39 @@
+var exports = module.exports = {};
+
+
+/*
+ * Helper function that returns a 4 byte buffer
+ * parameter: hex, a hex number
+ * returns : buffer representation of hex in int32 Little Endian Format
+ */
+function getInt32LE(hex){
+  var buf = new Buffer(4);
+  buf.writeUInt32LE(hex, 0);
+  return buf;
+}
+
 /* the dom0_client that we could require
  * and create/remove operations
  */
 
 var net = require('net'),
-    debug = require('./debug.js');
+    debug = require('./debug.js'),
+    sleep = require('node-sleep').sleep,
+    EventEmitter = require( "events" ).EventEmitter;
+    ;
+
 
 var dom0_client = function() {
-  var server_details, // JS Object, details of the client Server, host and port
-  clientSocket, // variable that holds the socket connection to the client
-  connected // BOOL, state of the connection
-  ;
-  var magic_numbers = require('./magic_numbers.js'); // this file has all the communication protocol IDs
+  this.server_details = {}; // JS Object, details of the client Server, host and port
+  this.clientSocket = null; // variable that holds the socket connection to the client
+  this.monitorSocket = null;
+  this.connected = false;// BOOL, state of the connection
+  this.magic_numbers = require('./magic_numbers.js'); // this file has all the communication protocol IDs
 };
+
+// make the dom0 client inherit all the properties
+// of eventEmitter
+dom0_client.prototype = new EventEmitter;
 
 /*
  * function init: initializes the communication and sets the channels
@@ -21,10 +43,32 @@ var dom0_client = function() {
  *    host
  *
  */
-dom0_client.prototype.init = function(server_details) {
+dom0_client.prototype.init = function(options) {
+  var client = this;
   debug.log('Creating the client socket to talk to server');
-  // create the client
-  this.clientSocket = net.createConnection(server_details);
+
+  // create the client, connect to it
+  this.clientSocket = net.createConnection(options.server_details);
+
+  this.clientSocket.on('connect', function(){
+    client.emit('connected');
+  });
+  this.clientSocket.on('data', function(data) {
+    console.log( " == " + data.toString());
+  });
+
+  // start listening on a seperate port and create handlers for them as well.
+  //this.monitorSocket = net.createServer(function(socket){
+    //console.log('CONNECTED: '+ sock.remoteAddress + ':' + sock.remotePort);
+
+    //sock.on('data', function(data) {
+      //console.log('DATA ' + sock.remoteAddress + ': ' + data);
+    //});
+
+    //sock.on('close', function(data) {
+      //console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    //});
+  //}).listen(options.mon_client.port, options.mon_client.host);
 };
 
 /*
@@ -35,7 +79,8 @@ dom0_client.prototype.init = function(server_details) {
  */
 dom0_client.prototype.sendControl = function() {
   debug.log('sending control message to server ');
-  this.clientSocket.write(new Buffer(this.magic_numbers.control));
+
+  this.clientSocket.write(getInt32LE(this.magic_numbers.control));
 };
 
 /*
@@ -47,14 +92,19 @@ dom0_client.prototype.sendControl = function() {
 dom0_client.prototype.sendLua = function(message) {
   debug.log('sending lua meesage to the server '+ message);
 
-  // get the length of the message
-  var messageLength = message.length;
-
   // protocol is to send LUA control message first
   //                then send the length of the message
   //                then send the message
-  this.clientSocket.write(new Buffer(this.magic_numbers.lua));
-  this.clientSocket.write(messageLength);
+  //console.log(this.magic_numbers.lua);
+  //var lua = new Buffer(4);
+  //lua.writeUInt32LE(this.magic_numbers.lua, 0);
+  //this.clientSocket.write(lua);
+  this.clientSocket.write(getInt32LE(this.magic_numbers.lua));
+  console.log(getInt32LE);
+  debug.log('send message length ');
+  this.clientSocket.write(getInt32LE(message.length));
+  console.log(getInt32LE(message.length));
+  debug.log('send Message');
   this.clientSocket.write(message);
 
   // TODO: we should get a response if the LUA message was received
@@ -73,11 +123,23 @@ dom0_client.prototype.sendBinary = function(binary) {
   debug.log('sending binary file to the server');
 
   this.sendControl();
-  this.clientSocket.write(this.magic_numbers.send_binary);
-  this.clientSocket.write(binary.length);
+
+  this.clientSocket.write(getInt32LE(this.magic_numbers.send_binary));
+  this.clientSocket.write(getInt32LE(binary.length));
+
+  debug.log('expecting an OK by this time');
+  sleep(3000);
 
   // TODO: wait for GO_SEND here. need to make these parts more elegant
-  this.clientSocket.write(binary
+  this.clientSocket.write(binary);
+};
+
+/*
+ * function close : closes the socket connection
+ *
+ */
+dom0_client.prototype.close = function(){
+  this.clientSocket.end();
 }
 
 module.exports = dom0_client;
--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-
 // setup the required modules
 var xml2json = require('xml2json'),
     fs = require('fs'),
-    dom0_client = require('./dom0_client.js'), // dom0_client to talk to the server
-    magic_numbers = require('./magic_numbers.js') // magic number for communication
+    // dom0_client to talk to the server
+    dom0_client = require('./dom0_client.js'),
+    // magic number for communication
+    magic_numbers = require('./magic_numbers.js'),
+    sleep = require('node-sleep').sleep
     ;
 
 // TODO: move these variables to the config.json / configManager
 var tasksFile = 'tasks.xml';
 var server_details = {
-    'host': '127.0.0.1',
-    'port': '3001'
-  };
+  'host': '192.168.0.13',
+  //'host': '127.0.0.1',
+  'port': '3001'
+};
+var monitor_client_details = {
+  'host' : '192.168.0.254',
+  'port' : '3002'
+};
 
 // read the XML data from the tasks.xml file
 // XXX: configure the filename in a config.json file in the end
@@ -36,6 +43,15 @@ tasks = JSON.parse(tasks);
 }*/
 var exampleTask = tasks.taskset.periodictask[0];
 
+var binary = fs.readFileSync('binary.elf');
+
 var client = new dom0_client();
-client.init(server_details);
-client.sendLua("print 'hello';");
+client.init({server_details: server_details, mon_client: monitor_client_details});
+
+client.on('connected', function(){
+  console.log('connected');
+  //client.sendLua("print 'hello';");
+  client.sendBinary(binary);
+  sleep(3000);
+  client.sendLua("test = L4.default_loader:start({caps={l4re_ipc = L4.Env.l4re_ipc}},\"network\");");
+});
